9a5cd485f0903ed39dc9ef6bf1622e2b7b3829e7
damvanhoangbuu1 <damhoangbuu@gmail.com>
2020-08-03 11:13:05 +0700

    Search by story name or author

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,6 @@
 class StaticPageController < ApplicationController
   def home
     @hot_stories = Story.order("views DESC").limit(6)
-    @stories = Story.order("created_at DESC").paginate(page: params[:page])
+    @stories = Story.search(params[:term]).paginate(page: params[:page])
   end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -4,4 +4,12 @@ class Story < ApplicationRecord
   has_many :chapters, dependent: :delete_all
   belongs_to :user
   belongs_to :category
+
+  def self.search term
+    if term
+      where("story_name LIKE :search OR author LIKE :search", search: "%#{term}%")
+    else
+      order("created_at DESC")
+    end
+  end
 end
